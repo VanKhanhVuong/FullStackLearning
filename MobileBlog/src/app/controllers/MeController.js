f256ac21ed9fac9b624a811aea387bde1beccc5f
VanKhanhVuong <vankhanhvuong98@gmail.com>
2024-09-03 08:58:28 +0700

    commit feature count deleted items

--- MobileBlog/src/app/controllers/MeController.js
+++ MobileBlog/src/app/controllers/MeController.js
@@ -1,28 +1,33 @@
 const BaseController = require("./BaseController");
 const Course = require("../models/Course");
-const { multipleMongooseToObject } = require('../../util/mongoose');
+const { multipleMongooseToObject } = require("../../util/mongoose");
 
 class MeController extends BaseController {
-  
   // [GET] /me/stored/courses
   storedCourses(req, res, next) {
-    Course.find({})
-    .then(courses => res.render('me/stored-courses', {
-       courses: multipleMongooseToObject(courses)
-    }))
-    .catch(err => next(err));
+    Promise.all([Course.find({}), Course.countDocumentsWithDeleted({deleted:true})])
+      .then(([courses, deleteCount]) =>
+        res.render("me/stored-courses", {
+          deleteCount,
+          courses: multipleMongooseToObject(courses),
+        })
+      )
+      .catch((err) => next(err));
   }
 
   // [GET] /me/trash/courses
   trashCourses(req, res, next) {
-    Course.findWithDeleted({deleted:true})
-      .then(courses => {
-        res.render('me/trash-courses', {
-          courses: multipleMongooseToObject(courses)
+    Course.findWithDeleted({ deleted: true })
+      .then((courses) => {
+        res.render("me/trash-courses", {
+          courses: multipleMongooseToObject(courses),
         });
       })
-      .catch(err => next(err));
+      .catch((err) => next(err));
   }
 }
 
 module.exports = new MeController();
+
+// Document:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
